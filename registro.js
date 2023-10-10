6257a78ae09bf0cb308054d4a0790c4e30eff7f5
Lucastcanto <lucastomascanto@gmail.com>
2023-10-10 08:40:17 -0300

    Vistas Registro y Logueo Terminadas

--- registro.js
+++ registro.js
@@ -1,11 +1,21 @@
-const wrapper = document.querySelector('.wrapper');
-const loginlink=document.querySelector('.login-link');
-const registerlink=document.querySelector('.register-link');
 
-registerlink.addEventListener('click',()=>{
-wrapper.classList.add('active');
-})
+/*document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("loginForm");
+    const loginBtn = document.getElementById("loginBtn");
+    const registerLink = document.getElementById("register-link");
+    const registerForm = document.getElementById("registerBox");
 
-loginlink.addEventListener('click',()=>{
-    wrapper.classList.remove('active');
-    })
+    loginBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        registerForm.style.display = "none";
+        loginForm.style.display = "block";
+    });
+
+
+    registerLink.addEventListener("click", function (e) {
+        e.preventDefault();
+        loginForm.style.display = "none";
+        registerForm.style.display = "block";
+    });
+});*/
+
